cecc14a403bc66f9c34bbb9629856209317b9a03
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-19 11:58:49 +0530

    increment and decrement operator

--- src/main/java/topics/operators/IncrementAndDecrementOperators.java
+++ src/main/java/topics/operators/IncrementAndDecrementOperators.java
@@ -5,8 +5,13 @@ public class IncrementAndDecrementOperators {
         int x = 10;
         System.out.println(x++);
         System.out.println(x);
-        
+
         System.out.println(++x);
         System.out.println(x);
+        int y =10;
+        System.out.println(--y);
+        System.out.println(y--);
+        System.out.println(y);
+
     }
 }
