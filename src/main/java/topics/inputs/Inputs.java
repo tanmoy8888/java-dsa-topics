fbf724b786805c377dbb1bede7312e71b4e8d8b8
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-19 10:41:04 +0530

    variables

--- src/main/java/topics/inputs/Inputs.java
+++ src/main/java/topics/inputs/Inputs.java
@@ -1,7 +1,11 @@
 package topics.inputs;
 
+import java.util.Scanner;
+
 public class Inputs {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println("i = " + i);
     }
 }
