a20f9b9b151367c2474f8a5e25e61928933ce0a6
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-19 12:34:43 +0530

    if else

--- src/main/java/topics/basics/conditionals/EvenOrOdd.java
+++ src/main/java/topics/basics/conditionals/EvenOrOdd.java
@@ -6,10 +6,7 @@ public class EvenOrOdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        if (number % 2 == 0) {
-            System.out.println(number + " is a even number");
-        } else {
-            System.out.println(number + " is a odd number");
-        }
+        if (number % 2 == 0) System.out.println(number + " is a even number");
+        else System.out.println(number + " is a odd number");
     }
 }
