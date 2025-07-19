f2b6c54016d669ba9603fb313d32718ffebc4ad0
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-19 10:28:13 +0530

    variables

--- src/main/java/topics/variables/Variables.java
+++ src/main/java/topics/variables/Variables.java
@@ -4,5 +4,11 @@ public class Variables {
     public static void main(String[] args) {
         int x =4;
         System.out.println(x + 7);
+        // There are below rules of variables
+        // 1 : variables name can not be start with a number
+        // 2 : variables can start with any alphabets and _ or $
+        // 3 : variables can not contain special chars like (! ,@ , # , ^ , % etc.)
+        // 4 : spaces are not allowed while giving a name in a variable
+        // 5 : keywords are not allowed as a variable name
     }
 }
