4d5533d85ca997046facc07f3c7d0ea7b79b8a9d
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-20 11:08:02 +0530

    if else condition

--- src/main/java/topics/basics/loops/PrintEvenNumber.java
+++ src/main/java/topics/basics/loops/PrintEvenNumber.java
@@ -2,14 +2,14 @@ package topics.basics.loops;
 
 public class PrintEvenNumber {
     public static void main(String[] args) {
-    /*    for (int i = 0; i <=100 ; i=i+2) {
-            System.out.println("i = " + i);
-        }*/
-        // Another approach
-        for (int i = 0; i <=100 ; i++) {
+        /*for (int i = 0; i <=100 ; i++) {
             if(i%2 ==0){
                 System.out.println("i = " + i);
             }
+        }*/
+        // Another approach
+        for (int i = 0; i <= 100; i = i + 2) {
+            System.out.println("i = " + i);
         }
     }
 }
