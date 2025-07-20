d3d49e4b08d01764f52a47568de674fa6979d6ce
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-20 11:17:55 +0530

    loop

---
+++ src/main/java/topics/basics/loops/TableOf19.java
@@ -0,0 +1,9 @@
+package topics.basics.loops;
+
+public class TableOf19 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println("i = " + 19*i);
+        }
+    }
+}
