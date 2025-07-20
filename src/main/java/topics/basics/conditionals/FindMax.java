ff3a249d927f7d5c6e31994af82e1135e1e17d41
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-20 09:51:15 +0530

    if else condition

---
+++ src/main/java/topics/basics/conditionals/FindMax.java
@@ -0,0 +1,23 @@
+package topics.basics.conditionals;
+
+import java.util.Scanner;
+
+public class FindMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number.");
+        int first = sc.nextInt();
+        System.out.println("Enter second number.");
+        int second = sc.nextInt();
+        System.out.println("Enter third number.");
+        int third = sc.nextInt();
+        if (first > second && first > third) {
+            System.out.println(first + " first is greatest");
+        } else if (second > first && second > third) {
+            System.out.println(+second + " second is greatest");
+        } else {
+            System.out.println(third + " third is greatest");
+        }
+
+    }
+}
