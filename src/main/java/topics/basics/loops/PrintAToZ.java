34b1223256016516786fd041b4634b6fcbd5a5c1
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-20 11:25:02 +0530

    loop

---
+++ src/main/java/topics/basics/loops/PrintAToZ.java
@@ -0,0 +1,10 @@
+package topics.basics.loops;
+
+public class PrintAToZ {
+    public static void main(String[] args) {
+        for (int i = 65; i <=90 ; i++) {
+            char c = (char) i;
+            System.out.print(c);
+        }
+    }
+}
