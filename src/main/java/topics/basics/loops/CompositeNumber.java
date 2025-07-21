9c956bfbed97ca57e229be2ac5fe19236114736e
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-21 20:43:21 +0530

    loop

---
+++ src/main/java/topics/basics/loops/CompositeNumber.java
@@ -0,0 +1,17 @@
+package topics.basics.loops;
+
+import java.util.Scanner;
+
+public class CompositeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean composite = false;
+        for (int i = 2; i < n-1; i++) {
+            if(n%i ==0){
+                composite =true;
+            }
+        }
+        System.out.println("composite = " + composite);
+    }
+}
