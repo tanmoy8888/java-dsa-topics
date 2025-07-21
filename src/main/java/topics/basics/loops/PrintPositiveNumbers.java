2467862cf24aa12fb892a70658c4f4c43515b212
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-21 20:40:34 +0530

    loop

---
+++ src/main/java/topics/basics/loops/PrintPositiveNumbers.java
@@ -0,0 +1,9 @@
+package topics.basics.loops;
+
+public class PrintPositiveNumbers {
+    public static void main(String[] args) {
+        for (int i = 99 ; i >0 ; i=i-4) {
+            System.out.println("i = " + i);
+        }
+    }
+}
