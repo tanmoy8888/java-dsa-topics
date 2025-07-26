3e1d1bf9bb8120c11a470d04f121528c2f6ef0c2
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-26 20:07:57 +0530

    pattern

--- src/main/java/topics/basics/patterns/practice/StarPlus_7.java
+++ src/main/java/topics/basics/patterns/practice/StarPlus_7.java
@@ -2,7 +2,7 @@ package topics.basics.patterns.practice;
 
 public class StarPlus_7 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 7;
         int middle = n / 2 + 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
