c53e78cafcdcffd7f01543526e680486c2b05712
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-26 19:23:39 +0530

    pattern

--- src/main/java/topics/basics/patterns/practice/Pattern_4.java
+++ src/main/java/topics/basics/patterns/practice/Pattern_4.java
@@ -8,20 +8,21 @@ import java.util.Scanner;
  O/P =>
 
  1
- 2 2
+ A B
  3 3 3
- 4 4 4 4
- 5 5 5 5 5
+ A B C D
+ 5 5 5 5 5  
 
  */
 
-public class Pattern_3 {
+public class Pattern_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(+i+" ");
+                if(i%2 ==0) System.out.print((char) (64+j)+" ");
+                else System.out.print(+i+" ");
             }
             System.out.println();
         }
