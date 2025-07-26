2bb90ef5affa014519fea87bba6407c9720eb8ac
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-26 22:18:35 +0530

    pattern

--- src/main/java/topics/basics/patterns/practice/Pattern_Important_13.java
+++ src/main/java/topics/basics/patterns/practice/Pattern_Important_13.java
@@ -10,6 +10,9 @@ public class Pattern_Important_13 {
             for (int j = 1; j <=i ; j++) {
                 System.out.print(j+" ");
             }
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+" ");
+            }
 
             System.out.println();
         }
