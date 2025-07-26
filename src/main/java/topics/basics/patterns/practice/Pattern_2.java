103e08f69ed369794f91bf9df243ca68df7b072d
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-26 19:18:54 +0530

    pattern

---
+++ src/main/java/topics/basics/patterns/practice/Pattern_2.java
@@ -0,0 +1,29 @@
+package topics.basics.patterns.practice;
+
+import java.util.Scanner;
+
+/**
+ * Print this below pattern.
+
+ O/P =>
+
+ 1
+ 1 2
+ 1 2 3
+ 1 2 3 4
+ 1 2 3 4 5 
+
+ */
+
+public class Pattern_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(+j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
