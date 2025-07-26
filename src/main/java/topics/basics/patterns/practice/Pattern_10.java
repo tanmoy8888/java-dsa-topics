d8d958b777d8e76d92cc9245169ed25e5c8be963
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-26 21:06:01 +0530

    pattern

--- src/main/java/topics/basics/patterns/practice/Pattern_10.java
+++ src/main/java/topics/basics/patterns/practice/Pattern_10.java
@@ -3,20 +3,20 @@ package topics.basics.patterns.practice;
 import java.util.Scanner;
 
 /**
-
- Print this below pattern
-
- 1
- 1 3
- 1 3 5
- 1 3 5 7
-
+ * Print this below pattern
+ * <p>
+ * 1
+ * 1 3
+ * 1 3 5
+ * 1 3 5 7
  */
 
 public class Pattern_10 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        // First approach
+
         /*for (int i = 1; i <=n ; i++) {
             int a = 1;
             for (int j = 1; j <=i;j++) {
@@ -26,10 +26,12 @@ public class Pattern_10 {
             System.out.println();
         }*/
 
-        for (int i = 1; i <=n ; i++) {
-            for (int j = 1; j <=i;j++) {
-                if(j > 1) System.out.print((2*j-1)+" ");
-                else System.out.print(j+" ");
+        // Second approach -- without using extra variable
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j > 1) System.out.print((2 * j - 1) + " ");
+                else System.out.print(j + " ");
             }
             System.out.println();
         }
