ad11b68939a9e951c510a973a2068891dbdf4616
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-26 12:00:35 +0530

    pattern

---
+++ src/main/java/topics/basics/patterns/Pattern_8.java
@@ -0,0 +1,13 @@
+package topics.basics.patterns;
+
+public class Pattern_8 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
