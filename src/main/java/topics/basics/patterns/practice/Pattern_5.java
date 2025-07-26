7375bfabbb1082d6ba754eabe2fdc0736b42b609
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-26 19:34:09 +0530

    pattern

--- src/main/java/topics/basics/patterns/practice/Pattern_5.java
+++ src/main/java/topics/basics/patterns/practice/Pattern_5.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 
 /**
  * Print this below pattern.
-
- O/P =>
-
+ * <p>
+ * O/P =>
+ * <p>
  * * * * *
  * * * *
  * * *
  * *
- *
-
  */
 
 public class Pattern_5 {
@@ -20,12 +18,25 @@ public class Pattern_5 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // First approach
-        
+
        /* for (int i = 1; i <= n; i++) {
             for (int j = n; j >= i; j--) {
                 System.out.print("*"+" ");
             }
             System.out.println();
         }*/
+
+        // Second approach
+        /*
+          // very important
+          i+j = n+1
+          j = n+1-i
+         */
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n + 1 - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
