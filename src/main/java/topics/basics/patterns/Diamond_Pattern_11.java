11b43f306d78674eda741caf8615c45b711a264d
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-07-29 19:34:42 +0530

    pattern

--- src/main/java/topics/basics/patterns/Diamond_Pattern_11.java
+++ src/main/java/topics/basics/patterns/Diamond_Pattern_11.java
@@ -6,7 +6,7 @@ public class Diamond_Pattern_11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int nsp = n;
+        int nsp = n-1;
         int nst = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= nsp; j++) {
@@ -15,14 +15,23 @@ public class Diamond_Pattern_11 {
             for (int j = 1; j <= nst; j++) {
                 System.out.print("* ");
             }
-            for (int j = 1; j < nst; j++) {
+            System.out.println();
+            nsp--;
+            nst += 2;
+        }
+        nst = nst - 4;
+        nsp = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print(" " + " ");
+            }
+            for (int j = 1; j <= nst; j++) {
                 System.out.print("* ");
             }
             System.out.println();
-            nsp--;
-            nst++;
+            nsp++;
+            nst -= 2;
         }
-
 
     }
 
