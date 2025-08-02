6a340c103f030e46d27f2673def59ac76dd384bc
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-02 10:22:44 +0530

    array

---
+++ src/main/java/topics/arrays/MaxElementOfAnArray.java
@@ -0,0 +1,12 @@
+package topics.arrays;
+
+public class MaxElementOfAnArray {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6};
+        int max = arr[0];
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i] > max) max = arr[i];
+        }
+        System.out.println("max = " + max);
+    }
+}
