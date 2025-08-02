d25152f92e779d2b1291dbd035811aa8c79b9e94
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-02 11:21:50 +0530

    array

--- src/main/java/topics/arrays/BuiltInArrayMethods.java
+++ src/main/java/topics/arrays/BuiltInArrayMethods.java
@@ -5,7 +5,11 @@ import java.util.Arrays;
 public class BuiltInArrayMethods {
     public static void main(String[] args) {
         int[] arr = {5, 3, 4, 1, 6, 7, 1};
-        Arrays.sort(arr);
+
+        /*Arrays.sort(arr);
+        printArray(arr);*/
+
+        Arrays.sort(arr,2,4);
         printArray(arr);
     }
 
