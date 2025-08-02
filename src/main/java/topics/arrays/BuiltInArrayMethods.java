6295c4c283535448f2db51e1412f689a21a93395
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-02 11:19:56 +0530

    array

---
+++ src/main/java/topics/arrays/BuiltInArrayMethods.java
@@ -0,0 +1,17 @@
+package topics.arrays;
+
+import java.util.Arrays;
+
+public class BuiltInArrayMethods {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 4, 1, 6, 7, 1};
+        Arrays.sort(arr);
+        printArray(arr);
+    }
+
+    private static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
