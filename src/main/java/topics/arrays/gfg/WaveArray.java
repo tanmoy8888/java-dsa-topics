fb920d3cc952639f97d803315ff29f9b80f403da
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-03 13:47:25 +0530

    array

--- src/main/java/topics/arrays/gfg/WaveArray.java
+++ src/main/java/topics/arrays/gfg/WaveArray.java
@@ -3,7 +3,7 @@ package topics.arrays.gfg;
 import java.util.Arrays;
 
 /**
- * 
+ *
  * GFG : Given an sorted array arr[] of integers. Sort the array into a wave-like array(In Place).
  * In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5] ..... and so on.
  * If there are multiple solutions, find the lexicographically smallest one.
---
+++ src/main/java/topics/arrays/leetcode/SortArrayByParity.java
@@ -0,0 +1,7 @@
+package topics.arrays.leetcode;
+
+public class SortArrayByParity {
+    public static void main(String[] args) {
+        
+    }
+}
