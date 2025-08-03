b0f5c514927b6b6fb28ca72fb6782f43a4cb3ecf
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-03 13:13:12 +0530

    array

---
+++ src/main/java/topics/arrays/gfg/WaveArray.java
@@ -0,0 +1,17 @@
+package topics.arrays.gfg;
+
+/**
+ *  GFG : Given an sorted array arr[] of integers. Sort the array into a wave-like array(In Place). In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5] ..... and so on. If there are multiple solutions, find the lexicographically smallest one.
+ *
+ * Note: The given array is sorted in ascending order, and modify the given array in-place without returning a new array.
+ *
+ *
+ */
+public class WaveArray {
+    public static void main(String[] args) {
+       int [] arr = {1, 2, 3, 4, 5};
+    }
+    public void sortInWave(int arr[]) {
+
+    }
+}
