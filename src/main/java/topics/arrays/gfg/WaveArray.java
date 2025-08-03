49cbfd9f2e1f24ba11022ef9daec3335a4de2cfb
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-03 13:29:49 +0530

    array

--- src/main/java/topics/arrays/gfg/WaveArray.java
+++ src/main/java/topics/arrays/gfg/WaveArray.java
@@ -1,19 +1,37 @@
 package topics.arrays.gfg;
 
+import java.util.Arrays;
+
 /**
- *  GFG : Given an sorted array arr[] of integers. Sort the array into a wave-like array(In Place).
- *  In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5] ..... and so on.
- *  If there are multiple solutions, find the lexicographically smallest one.
- *
+ * GFG : Given an sorted array arr[] of integers. Sort the array into a wave-like array(In Place).
+ * In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5] ..... and so on.
+ * If there are multiple solutions, find the lexicographically smallest one.
+ * <p>
  * Note: The given array is sorted in ascending order, and modify the given array in-place without returning a new array.
- *
- *
  */
 public class WaveArray {
     public static void main(String[] args) {
-       int [] arr = {1, 2, 3, 4, 5};
+        //int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {2, 4, 7, 8, 9, 10};
+        sortInWave(arr);
     }
-    public void sortInWave(int arr[]) {
 
+    public static void sortInWave(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+        int temp = 0;
+        while (start < end) {
+            if (start > 0 && arr[start - 1] < arr[start]) {
+                temp = arr[start - 1];
+                arr[start - 1] = arr[start];
+                arr[start] = temp;
+            } else if (start < end && arr[start] < arr[start + 1]) {
+                temp = arr[start];
+                arr[start] = arr[start + 1];
+                arr[start + 1] = temp;
+            }
+            start += 2;
+        }
+        System.out.println("Output = " + Arrays.toString(arr));
     }
 }
