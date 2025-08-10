26e1bda2887b6aa010dab1616b5a14c469fad9f1
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-10 10:47:41 +0530

    array

--- src/main/java/topics/arrays/two_pointers/ColoumnWisePrint.java
+++ src/main/java/topics/arrays/two_pointers/ColoumnWisePrint.java
@@ -3,8 +3,15 @@ package topics.arrays.two_pointers;
 public class ColoumnWisePrint {
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        for (int i = 0; i < arr.length; i++) {
+      /*  for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }*/
+
+        for (int j = 0; j < arr[0].length; j++) {
+            for (int i = 0; i < arr.length; i++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
