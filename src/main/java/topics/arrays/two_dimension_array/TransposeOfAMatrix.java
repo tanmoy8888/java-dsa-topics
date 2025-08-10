9c7d34f7c863c5c32162d28e233f26bc086ee507
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-10 11:43:08 +0530

    array

--- src/main/java/topics/arrays/two_dimension_array/TransposeOfAMatrix.java
+++ src/main/java/topics/arrays/two_dimension_array/TransposeOfAMatrix.java
@@ -3,6 +3,16 @@ package topics.arrays.two_dimension_array;
 public class TransposeOfAMatrix {
     public static void main(String[] args) {
         int[][] arr = {{2, 8, 3, 4}, {7, 2, 1, 6}, {5, 5, 4, 1}, {3, 1, 8, 2}};
+         printArray(arr);
+        System.out.println("Printing transpose matrix");
+        for (int i = 0; i < arr.length; i++) {
+            int temp = 0;
+            for (int j = 0; j < i; j++) {
+                temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
         printArray(arr);
     }
 
