bb6fe570589ebe9b2ed900a80feb1f7ba3a9e3fe
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-16 09:38:43 +0530

    array

--- src/main/java/topics/arrays/leetcode/MergeTwoSortedArray.java
+++ src/main/java/topics/arrays/leetcode/MergeTwoSortedArray.java
@@ -2,6 +2,18 @@ package topics.arrays.leetcode;
 
 public class MergeTwoSortedArray {
     public static void main(String[] args) {
-        
+        int [] nums1 = {1,2,3,0,0,0};
+        int [] nums2 = {2,5,6};
+        int m = nums1.length;
+        int n = nums2.length;
+        merge(nums1,m,nums2,n);
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int [] arr = new int[m+n];
+        int k =0;
+        while (k <= (m+n)-2){
+
+        }
+
     }
 }
