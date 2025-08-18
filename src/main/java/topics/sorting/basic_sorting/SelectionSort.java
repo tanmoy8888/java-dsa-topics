37b059c133dd5f408c6d86f88ccec66225d6293d
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-18 14:32:19 +0530

    sorting - bubble sort

--- src/main/java/topics/sorting/basic_sorting/SelectionSort.java
+++ src/main/java/topics/sorting/basic_sorting/SelectionSort.java
@@ -10,10 +10,9 @@ public class SelectionSort {
 
     private static void selectionSort(int[] arr) {
         int temp = 0;
-
         for (int i = 0; i < arr.length - 1; i++) {
-            int min = Integer.MAX_VALUE;
-            int minIndex = 0;
+            int min = arr[i];
+            int minIndex = i;
             for (int j = i ; j < arr.length; j++) {
                 if (arr[j] < min) {
                     min = arr[j];
