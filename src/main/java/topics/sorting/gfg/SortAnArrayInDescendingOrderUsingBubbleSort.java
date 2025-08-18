a1727cef7ccf13b950446fe4e625e4135592a4ce
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-18 13:17:32 +0530

    sorting - bubble sort

---
+++ src/main/java/topics/sorting/gfg/SortAnArrayInDescendingOrderUsingBubbleSort.java
@@ -0,0 +1,24 @@
+package topics.sorting.gfg;
+
+import java.util.Arrays;
+
+public class SortAnArrayInDescendingOrderUsingBubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 2, 5, 3};
+        sortInDescendingOrderUsingBubbleSort(arr);
+    }
+
+    private static void sortInDescendingOrderUsingBubbleSort(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Printing array = " + Arrays.toString(arr));
+    }
+}
