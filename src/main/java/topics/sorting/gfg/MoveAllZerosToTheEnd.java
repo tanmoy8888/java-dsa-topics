992326908ec2037226eae1e465b5253fce5de0d7
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-18 13:38:36 +0530

    sorting - bubble sort

---
+++ src/main/java/topics/sorting/gfg/MoveAllZerosToTheEnd.java
@@ -0,0 +1,24 @@
+package topics.sorting.gfg;
+
+import java.util.Arrays;
+
+public class MoveAllZerosToTheEnd {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+        pushZerosToEnd(arr);
+    }
+
+    private static void pushZerosToEnd(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] == 0) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Printing array = " + Arrays.toString(arr));
+    }
+}
