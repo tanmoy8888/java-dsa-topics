f4fbd52f79b4bf086cb70ab7b6fb6f685161e7bb
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-18 19:19:00 +0530

    sorting - selection sort

--- src/main/java/topics/sorting/gfg/CommonElements.java
+++ src/main/java/topics/sorting/gfg/CommonElements.java
@@ -5,34 +5,40 @@ import java.util.Arrays;
 
 
 /**
- *  https://www.geeksforgeeks.org/problems/common-elements5420/1
- *  Given two integer arrays a[] and b[]. Return the list of elements common to both the lists
- *  and return the list in sorted order. Duplicates may be there in the output list.
- *
- *  Input: a[] = [3, 4, 2, 2, 4] , b[] = [3, 2, 2, 7]
+ * https://www.geeksforgeeks.org/problems/common-elements5420/1
+ * Given two integer arrays a[] and b[]. Return the list of elements common to both the lists
+ * and return the list in sorted order. Duplicates may be there in the output list.
+ * <p>
+ * Input: a[] = [3, 4, 2, 2, 4] , b[] = [3, 2, 2, 7]
  * Output: 2 2 3
  * Explanation: The common elements in sorted order are [2, 2, 3]
- *
+ * <p>
  * Input: a[] = [3, 6, 1, 7, 9, 8, 2, 2] , b[] = [9, 7, 3, 4, 9]
  * Output: 3 7 9
  * Explanation: The common elements in sorted order are [3, 7, 9]
- *
- *
  */
 
 public class CommonElements {
     public static void main(String[] args) {
-
+        int a[] = {3, 4, 2, 2, 4};
+        int b[] = {3, 2, 2, 7};
+        System.out.println("Output = " + commonElements(a, b));
     }
 
     public static ArrayList<Integer> commonElements(int a[], int b[]) {
         Arrays.sort(a);
         Arrays.sort(b);
-        int min = Math.min(a.length, b.length);
+        int i = 0, j = 0;
         ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < min - 1; i++) {
-            if (a[i] == b[i]) {
+        while (i < a.length && j < b.length) {
+            if (a[i] == b[j]) {
                 list.add(a[i]);
+                i++;
+                j++;
+            } else if (a[i] > b[j]) {
+                j++;
+            } else if (a[i] < b[j]) {
+                j++;
             }
         }
         return list;
