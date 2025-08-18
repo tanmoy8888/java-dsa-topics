a3e712a688382dda15705696a9c2cd1d7dea6758
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-18 12:25:13 +0530

    array

--- src/main/java/topics/arrays/leetcode/NonDecreasingArray.java
+++ src/main/java/topics/arrays/leetcode/NonDecreasingArray.java
@@ -1,4 +1,32 @@
 package topics.arrays.leetcode;
 
 public class NonDecreasingArray {
+    public static void main(String[] args) {
+        //int[] nums = {4, 2, 3};
+        //int[] nums = {4, 2, 1};
+        int[] nums = {3, 4, 2, 3};
+        System.out.println("checkPossibility(nums) = " + checkPossibility(nums));
+    }
+
+    public static boolean checkPossibility(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for (int i = 2; i < nums.length - 1; i++) {
+            if (nums[1] > nums[0]) max = nums[1];
+            else {
+                max = nums[0];
+                count++;
+            }
+            if (nums[i] < nums[i + 1]) {
+                max = nums[i + 1];
+            }
+            if (max > nums[i + 1]) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/topics/sorting/gfg/CheckIFArrayIsSorted.java
+++ src/main/java/topics/sorting/gfg/CheckIFArrayIsSorted.java
@@ -1,7 +1,21 @@
-package topics.sorting;
+package topics.sorting.gfg;
 
+/**
+ *  GFG : https://www.geeksforgeeks.org/problems/check-if-an-array-is-sorted0701/1
+ *  Check if array is sorted
+ */
 public class CheckIFArrayIsSorted {
     public static void main(String[] args) {
-
+     //int [] arr = {10, 20, 30, 40, 50};
+     int [] arr = {90, 80, 100, 70, 40, 30};
+        System.out.println("isSorted(arr) = " + isSorted(arr));
+    }
+    public static boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i] > arr[i+1]){
+                return false;
+            }
+        }
+        return true;
     }
 }
