e47768c35743395f91cb6d7688721019503ed7c1
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-22 21:45:26 +0530

    binary search

--- src/main/java/topics/binary_search/leetcode/FirstAndLastPositionOfASortedArray.java
+++ src/main/java/topics/binary_search/leetcode/FirstAndLastPositionOfASortedArray.java
@@ -12,7 +12,26 @@ import java.util.Arrays;
  You must write an algorithm with O(log n) runtime complexity.
 
 
- 
+ Example 1:
+
+ Input: nums = [5,7,7,8,8,10], target = 8
+ Output: [3,4]
+ Example 2:
+
+ Input: nums = [5,7,7,8,8,10], target = 6
+ Output: [-1,-1]
+ Example 3:
+
+ Input: nums = [], target = 0
+ Output: [-1,-1]
+
+
+ Constraints:
+
+ 0 <= nums.length <= 105
+ -109 <= nums[i] <= 109
+ nums is a non-decreasing array.
+
 
  */
 
