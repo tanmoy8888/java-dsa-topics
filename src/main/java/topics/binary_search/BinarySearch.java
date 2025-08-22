980a92d6479bfa74bd31d4df66a2ddc1e272ecab
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-22 19:35:31 +0530

    binary search

--- src/main/java/topics/binary_search/BinarySearch.java
+++ src/main/java/topics/binary_search/BinarySearch.java
@@ -1,22 +1,22 @@
-/*
 package topics.binary_search;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int [] nums = {-1,0,3,5,9,12};
+        int[] nums = {-1, 0, 3, 5, 9, 12};
         int target = 9;
         System.out.println("Output = " + search(nums, target));
 
     }
+
     public static int search(int[] nums, int target) {
-     int low = 0;
-     int high = nums.length-1;
-     int middle = (low/high)/2;
-     while (low <=high){
-
-         if(nums[middle] > target);
-     }
-
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            if (nums[middle] > target) high = middle - 1;
+            else if (nums[middle] < target) low = middle + 1;
+            else return middle;
+        }
+        return -1;
     }
 }
-*/
