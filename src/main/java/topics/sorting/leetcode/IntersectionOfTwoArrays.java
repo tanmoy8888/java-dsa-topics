c8be5848557dd50264a34c92d0863d393273d1ff
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-22 16:53:06 +0530

    sorting

---
+++ src/main/java/topics/sorting/leetcode/IntersectionOfTwoArrays.java
@@ -0,0 +1,33 @@
+package topics.sorting.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IntersectionOfTwoArrays {
+    public static void main(String[] args) {
+        int[] nums1 = {4, 9, 5};
+        int[] nums2 = {9, 4, 9, 8, 4};
+        System.out.println("intersect(nums1,nums2) = " + Arrays.toString(intersect(nums1, nums2)));
+    }
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        List<Integer> list = new ArrayList<>();
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+}
