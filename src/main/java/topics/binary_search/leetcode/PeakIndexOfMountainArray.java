506c9d6751cd3896c0a2793609b9b3160d354f4f
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-25 20:54:56 +0530

    binary search

--- src/main/java/topics/binary_search/leetcode/PeakIndexOfMountainArray.java
+++ src/main/java/topics/binary_search/leetcode/PeakIndexOfMountainArray.java
@@ -8,7 +8,33 @@ package topics.binary_search.leetcode;
  Return the index of the peak element.
 
  Your task is to solve it in O(log(n)) time complexity.
- 
+
+ Example 1:
+
+ Input: arr = [0,1,0]
+
+ Output: 1
+
+ Example 2:
+
+ Input: arr = [0,2,1,0]
+
+ Output: 1
+
+ Example 3:
+
+ Input: arr = [0,10,5,2]
+
+ Output: 1
+
+
+
+ Constraints:
+
+ 3 <= arr.length <= 105
+ 0 <= arr[i] <= 106
+ arr is guaranteed to be a mountain array.
+
  */
 
 public class PeakIndexOfMountainArray {
