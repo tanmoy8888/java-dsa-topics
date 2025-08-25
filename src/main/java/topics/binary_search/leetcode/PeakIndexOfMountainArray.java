244be216b7f29191535ae91ba27da38c33afe68c
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-25 20:50:10 +0530

    binary search

--- src/main/java/topics/binary_search/leetcode/PeakIndexOfMountainArray.java
+++ src/main/java/topics/binary_search/leetcode/PeakIndexOfMountainArray.java
@@ -2,14 +2,14 @@ package topics.binary_search.leetcode;
 
 public class PeakIndexOfMountainArray {
     public static void main(String[] args) {
-      //  int[] arr = {0, 2, 1, 0};
-        int[] arr = {3,4,5,1};
+        //  int[] arr = {0, 2, 1, 0};
+        int[] arr = {3, 4, 5, 1};
         System.out.println("peakIndexInMountainArray(arr) = " + peakIndexInMountainArray(arr));
     }
 
     public static int peakIndexInMountainArray(int[] arr) {
-        int low = 0;
-        int high = arr.length - 1;
+        int low = 1;
+        int high = arr.length - 2;
         int index = -1;
         while (low <= high) {
             int middle = (low + high) / 2;
@@ -17,9 +17,9 @@ public class PeakIndexOfMountainArray {
                 index = middle;
                 break;
             } else if (arr[middle] > arr[middle - 1]) {
-                high = middle - 1;
-            } else if (arr[middle] < arr[middle + 1]) {
                 low = middle + 1;
+            } else if (arr[middle] < arr[middle + 1]) {
+                high = middle - 1;
             }
         }
         return index;
