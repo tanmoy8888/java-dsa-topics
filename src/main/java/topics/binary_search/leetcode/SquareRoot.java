b1b2db329d31619d0a3deb183d44d30b4a3ee6fd
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-26 20:45:40 +0530

    binary search

--- src/main/java/topics/binary_search/leetcode/SquareRoot.java
+++ src/main/java/topics/binary_search/leetcode/SquareRoot.java
@@ -1,5 +1,13 @@
 package topics.binary_search.leetcode;
 
+/**
+
+ Leetcode : 69. Sqrt(x)
+
+ 
+
+ */
+
 public class SquareRoot {
     public static void main(String[] args) {
         int x = 8;
@@ -7,14 +15,14 @@ public class SquareRoot {
     }
 
     public static int mySqrt(int x) {
-        if (x <= 0) return 1;
+        if (x <= 0) return 0;
         else {
             int low = 0;
             int high = x;
             int sqrt = 1;
             while (low <= high) {
                 int middle = (low + high) / 2;
-                if (middle * middle <= x) {
+                if ((long)middle * middle <= x) {
                     sqrt = middle;
                     low = middle + 1;
                 } else {
