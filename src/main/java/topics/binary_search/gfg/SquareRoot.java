fa9ae0d2506fe0dced09e5a82716436586c515d3
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-26 20:33:39 +0530

    binary search

--- src/main/java/topics/binary_search/gfg/SquareRoot.java
+++ src/main/java/topics/binary_search/gfg/SquareRoot.java
@@ -1,5 +1,30 @@
 package topics.binary_search.gfg;
 
+/**
+ GFG : Square Root
+ https://www.geeksforgeeks.org/problems/square-root/1
+
+ Given a positive integer n, find the square root of n. If n is not a perfect square, then return the floor value.
+
+ Floor value of any number is the greatest Integer which is less than or equal to that number.
+
+ Examples:
+
+ Input: n = 4
+ Output: 2
+ Explanation: Since, 4 is a perfect square, so its square root is 2.
+ Input: n = 11
+ Output: 3
+ Explanation: Since, 11 is not a perfect square, floor of square root of 11 is 3.
+ Input: n = 1
+ Output: 1
+ Explanation: 1 is a perfect sqaure, so its square root is 1.
+ Constraints:
+ 1 ≤ n ≤ 3*104
+
+
+
+ */
 public class SquareRoot {
     public static void main(String[] args) {
         int n = 4;
@@ -14,7 +39,7 @@ public class SquareRoot {
             int middle = (low + high) / 2;
             if (middle * middle <= n) {
                 sqrt = middle;
-                low = middle + 1
+                low = middle + 1;
             } else {
                 high = middle - 1;
             }
