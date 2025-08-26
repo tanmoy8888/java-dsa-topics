881dfecdde5e094f824885f3480c968effdc71e7
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-26 18:56:45 +0530

    binary search

--- src/main/java/topics/binary_search/gfg/FindTheFloorOfAnArray.java
+++ src/main/java/topics/binary_search/gfg/FindTheFloorOfAnArray.java
@@ -11,7 +11,23 @@ package topics.binary_search.gfg;
 
  Note: In case of multiple occurrences of ceil of x, return the index of the last occurrence.
 
+ Examples
 
+ Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 5
+ Output: 1
+ Explanation: Largest number less than or equal to 5 is 2, whose index is 1.
+ Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 11
+ Output: 4
+ Explanation: Largest Number less than or equal to 11 is 10, whose indices are 3 and 4. The index of last occurrence is 4.
+ Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 0
+ Output: -1
+ Explanation: No element less than or equal to 0 is found. So, output is -1.
+ Constraints:
+ 1 ≤ arr.size() ≤ 106
+ 1 ≤ arr[i] ≤ 106
+ 0 ≤ x ≤ arr[n-1]
+
+ 
 
  */
 
