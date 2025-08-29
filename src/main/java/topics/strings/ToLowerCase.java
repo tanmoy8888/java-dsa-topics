d51e8fd3ff0591930a93a143dcc14dac545d32b1
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-29 21:36:35 +0530

    binary search

--- src/main/java/topics/strings/ToLowerCase.java
+++ src/main/java/topics/strings/ToLowerCase.java
@@ -3,6 +3,8 @@ package topics.strings;
 /**
  Leetcode : 709. To Lower Case
 
+ Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
  
  */
 
