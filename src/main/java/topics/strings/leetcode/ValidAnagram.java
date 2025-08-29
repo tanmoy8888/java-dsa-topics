6a8d03360aabeb133c9b3dfcb5deee033120d5fb
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-08-29 21:56:12 +0530

    String

--- src/main/java/topics/strings/leetcode/ValidAnagram.java
+++ src/main/java/topics/strings/leetcode/ValidAnagram.java
@@ -1,5 +1,7 @@
 package topics.strings.leetcode;
 
+import java.util.Arrays;
+
 public class ValidAnagram {
     public static void main(String[] args) {
         String s = "rat", t = "car";
@@ -7,6 +9,16 @@ public class ValidAnagram {
     }
 
     public static boolean isAnagram(String s, String t) {
-       
+        if (s.length() != t.length()) return false;
+        else {
+            char[] c1 = s.toCharArray();
+            Arrays.sort(c1);
+            char[] c2 = t.toCharArray();
+            Arrays.sort(c2);
+            for (int i = 0; i < c1.length; i++) {
+                if (c1[i] != c2[i]) return false;
+            }
+            return true;
+        }
     }
 }
