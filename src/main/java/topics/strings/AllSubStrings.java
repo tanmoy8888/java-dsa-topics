3e596da9f32d828db29050513c7275bca456f974
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-09-02 19:46:52 +0530

    String

--- src/main/java/topics/strings/AllSubStrings.java
+++ src/main/java/topics/strings/AllSubStrings.java
@@ -2,6 +2,13 @@ package topics.strings;
 
 public class AllSubStrings {
     public static void main(String[] args) {
-
+        String s = "abc";
+        for (int i = 0; i < s.length(); i++) {
+            String combination = "";
+            for (int j = i; j < s.length(); j++) {
+                combination += s.charAt(j);
+                System.out.println(combination);
+            }
+        }
     }
 }
