1eae6d447e556a7be502b9635b80f572c93d0dd2
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2025-09-05 23:44:38 +0530

    String

---
+++ src/main/java/topics/strings/leetcode/StringCompression.java
@@ -0,0 +1,32 @@
+package topics.strings.leetcode;
+
+import java.util.Arrays;
+
+public class StringCompression {
+    public static void main(String[] args) {
+        char[] chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+        System.out.println("compress(chars) = " + compress(chars));
+    }
+
+    public static int compress(char[] chars) {
+        if (chars.length == 1) return 1;
+        else {
+            StringBuilder sb = new StringBuilder();
+            int i = 0, j = 0;
+            int frequency = 0;
+            Arrays.sort(chars);
+            while (j < chars.length) {
+                if (chars[i] == chars[j]) {
+                    j++;
+                } else {
+                    frequency = j - i;
+                    sb.append(chars[i] + String.valueOf(frequency));
+                    i = j;
+                }
+            }
+            frequency = j - i;
+            char[] arr = sb.append(chars[i] + String.valueOf(frequency)).toString().toCharArray();
+            return arr.length;
+        }
+    }
+}
