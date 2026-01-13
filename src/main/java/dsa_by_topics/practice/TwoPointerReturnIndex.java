a9ac4472a8f07e5449d2d3606609081e36ef0e50
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-13 21:41:26 +0530

    two pointer practice

---
+++ src/main/java/dsa_by_topics/practice/TwoPointerReturnIndex.java
@@ -0,0 +1,14 @@
+package dsa_by_topics.practice;
+
+import java.util.Arrays;
+
+public class TwoPointerReturnIndex {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 9, 1, 3};
+        int target = 6;
+        System.out.println("twoPointer(arr,target) = " + Arrays.toString(findIndex(arr, target)));
+    }
+    private static int[] findIndex(int [] arr , int target){
+        
+    }
+}
