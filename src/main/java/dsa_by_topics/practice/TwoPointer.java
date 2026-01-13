7d321b7e7a83c74595abd0c6cba078da50572b68
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-13 21:38:51 +0530

    teo pointer practice

--- src/main/java/dsa_by_topics/practice/TwoPointer.java
+++ src/main/java/dsa_by_topics/practice/TwoPointer.java
@@ -4,20 +4,27 @@ import java.util.Arrays;
 
 public class TwoPointer {
     public static void main(String[] args) {
-        int [] arr = {4,5,2,1,3};
+        int[] arr = {4, 5, 9, 1, 3};
         int target = 6;
+        System.out.println("twoPointer(arr,target) = " + Arrays.toString(twoPointer(arr, target)));
     }
-    private static int[] twoPointer(int[] arr , int target){
-        int [] result = new int[2];
-        int start = 0 ;
-        int end = arr.length-1;
+
+    private static int[] twoPointer(int[] arr, int target) {
+        int[] result = new int[2];
+        int start = 0;
+        int end = arr.length - 1;
         Arrays.sort(arr);
-        while (start>end){
-            if((arr[start]+arr[end]) == target){
+        while (start < end) {
+            if ((arr[start] + arr[end]) == target) {
                 result[0] = arr[start];
                 result[1] = arr[end];
-                return result;
+                break;
+            } else if ((arr[start] + arr[end]) > target) {
+                end--;
+            } else if ((arr[start] + arr[end]) < target) {
+                start++;
             }
         }
+        return result;
     }
 }
