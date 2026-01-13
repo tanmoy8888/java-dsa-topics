fc7f2df6694e46346fb3b32d6961c7efadccc512
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-13 21:27:56 +0530

    teo pointer practice

--- src/main/java/dsa_by_topics/practice/TwoPointer.java
+++ src/main/java/dsa_by_topics/practice/TwoPointer.java
@@ -1,5 +1,23 @@
-package dsa.by_topics.practice;
+package dsa_by_topics.practice;
+
+import java.util.Arrays;
 
 public class TwoPointer {
-
+    public static void main(String[] args) {
+        int [] arr = {4,5,2,1,3};
+        int target = 6;
+    }
+    private static int[] twoPointer(int[] arr , int target){
+        int [] result = new int[2];
+        int start = 0 ;
+        int end = arr.length-1;
+        Arrays.sort(arr);
+        while (start>end){
+            if((arr[start]+arr[end]) == target){
+                result[0] = arr[start];
+                result[1] = arr[end];
+                return result;
+            }
+        }
+    }
 }
