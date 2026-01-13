16309eb4b39c9d8bd22664f71934d7e2feefce89
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-13 22:41:37 +0530

    two pointer practice

--- src/main/java/dsa_by_topics/practice/RemoveDuplicateFromSortedArray.java
+++ src/main/java/dsa_by_topics/practice/RemoveDuplicateFromSortedArray.java
@@ -1,4 +1,25 @@
 package dsa_by_topics.practice;
 
+import java.util.Arrays;
+
 public class RemoveDuplicateFromSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 2, 3};
+        System.out.println("result[] = " + Arrays.toString(result(arr)));
+    }
+
+    private static int[] result(int[] arr) {
+        int start = 0;
+        int second = 1;
+        while (second <= arr.length - 1) {
+            if (arr[second] == arr[second - 1]) {
+                second++;
+            } else {
+                start = start+1;
+                arr[start] = arr[second];
+                second++;
+            }
+        }
+        return arr;
+    }
 }
