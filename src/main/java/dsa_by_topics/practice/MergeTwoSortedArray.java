3a8b796ca097ce48b8d2aea09210c2dd6448d221
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-14 21:03:39 +0530

    Two pointer practice

--- src/main/java/dsa_by_topics/practice/MergeTwoSortedArray.java
+++ src/main/java/dsa_by_topics/practice/MergeTwoSortedArray.java
@@ -15,13 +15,26 @@ public class MergeTwoSortedArray {
         int n = 3;
         MergeTwoSortedArray merge = new MergeTwoSortedArray();
         merge.merge(nums1, m, nums2, n);*/
+
+        // Second case
+        int[] nums1 = {0};
+        int m = 0;
+        int[] nums2 = {1};
+        int n = 1;
+        MergeTwoSortedArray merge = new MergeTwoSortedArray();
+        merge.merge(nums1, m, nums2, n);
     }
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = 0;
         int j = 0;
         int k = 0;
-        int[] temp = Arrays.copyOfRange(nums1, 0, m);
+        int[] temp = null;
+        if (m == 0) {
+            temp = new int[1];
+        } else {
+            temp = Arrays.copyOfRange(nums1, 0, m);
+        }
         while (i < m && j < n) {
             if (temp[i] < nums2[j]) {
                 nums1[k] = temp[i];
