a85b6b7a49cd18f81b1c022c4536bce6cc416c8d
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-14 22:30:38 +0530

    Two pointer practice

--- src/main/java/dsa_by_topics/practice/leetcode/SquareOfSortedArray.java
+++ src/main/java/dsa_by_topics/practice/leetcode/SquareOfSortedArray.java
@@ -12,14 +12,15 @@ public class SquareOfSortedArray {
 
     public int[] sortedSquares(int[] nums) {
         int m = 0;
-        int n = 0;
+        int first = 0;
+        int second = 0;
         int k = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] < 0) {
                 m = m + 1;
             }
         }
-        int[] negative = Arrays.copyOfRange(nums, 0, m+1);
+        int[] negative = Arrays.copyOfRange(nums, 0, m + 1);
         System.out.println("Arrays.toString(negative) = " + Arrays.toString(negative));
         int[] positive = Arrays.copyOfRange(nums, m + 1, nums.length);
         System.out.println("Arrays.toString(positive) = " + Arrays.toString(positive));
@@ -29,8 +30,8 @@ public class SquareOfSortedArray {
         System.out.println("Arrays.toString(negative) after square = " + Arrays.toString(negative));
         int[] negative_reverse = new int[negative.length];
         int j = 0;
-        for (int i = negative.length-1; i >= 0; i--) {
-            System.out.println("negative[i] = "+i+" : " + negative[i]);
+        for (int i = negative.length - 1; i >= 0; i--) {
+            System.out.println("negative[i] = " + i + " : " + negative[i]);
             negative_reverse[j] = negative[i];
             j++;
         }
@@ -39,24 +40,26 @@ public class SquareOfSortedArray {
             positive[i] = positive[i] * positive[i];
         }
         System.out.println("Arrays.toString(positive) = " + Arrays.toString(positive));
-        while (m < negative_reverse.length && n < positive.length) {
-            if (negative_reverse[m] < positive[n]) {
-                nums[k] = negative_reverse[m];
-                m++;
+        System.out.println("negative_reverse.length = " + negative_reverse.length);
+        System.out.println("positive.length = " + positive.length);
+        while (first < negative_reverse.length && second < positive.length) {
+            if (negative_reverse[first] < positive[second]) {
+                nums[k] = negative_reverse[first];
+                first++;
             } else {
-                nums[k] = positive[n];
-                n++;
+                nums[k] = positive[second];
+                second++;
             }
             k++;
         }
-        while (m < negative_reverse.length) {
-            nums[k] = negative_reverse[m];
-            m++;
+        while (first < negative_reverse.length) {
+            nums[k] = negative_reverse[first];
+            first++;
             k++;
         }
-        while (n < positive.length) {
-            nums[k] = positive[n];
-            n++;
+        while (second < positive.length) {
+            nums[k] = positive[second];
+            second++;
             k++;
         }
         return nums;
