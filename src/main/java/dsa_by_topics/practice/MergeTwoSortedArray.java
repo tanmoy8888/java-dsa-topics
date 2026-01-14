f4c022bb6df9baaa1a21d74e389140d01e2d010f
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-14 20:52:06 +0530

    String

---
+++ src/main/java/dsa_by_topics/practice/MergeTwoSortedArray.java
@@ -0,0 +1,47 @@
+package dsa_by_topics.practice;
+
+import java.util.Arrays;
+
+/**
+ * Leetcode 88. Merge Sorted Array
+ */
+
+public class MergeTwoSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        MergeTwoSortedArray merge = new MergeTwoSortedArray();
+        merge.merge(nums1, m, nums2, n);
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] temp = Arrays.copyOfRange(nums1, 0, m);
+        while (i < m && j < n) {
+            if (temp[i] < nums2[j]) {
+                nums1[k] = temp[i];
+                i++;
+            } else {
+                nums1[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < m) {
+            nums1[k] = temp[i];
+            i++;
+            k++;
+        }
+        while (j < n) {
+            nums1[k] = temp[j];
+            j++;
+            k++;
+        }
+        System.out.println("Arrays.toString(nums1) = " + Arrays.toString(nums1));
+    }
+
+}
