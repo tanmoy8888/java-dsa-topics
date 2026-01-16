c537e9d642a5b9dea62dc35c9a107e14d89324f7
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-16 23:12:37 +0530

    Two pointer practice

--- src/main/java/dsa_by_topics/practice/leetcode/SumOfTripletEqualsToZero.java
+++ src/main/java/dsa_by_topics/practice/leetcode/SumOfTripletEqualsToZero.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 /**
  * Leetcode :  15. 3Sum
- *
+ * <p>
  * Link  :  https://leetcode.com/problems/3sum/description/
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: nums = [-1,0,1,2,-1,-4]
  * Output: [[-1,-1,2],[-1,0,1]]
  * Explanation:
@@ -20,29 +20,57 @@ import java.util.List;
  * The distinct triplets are [-1,0,1] and [-1,-1,2].
  * Notice that the order of the output and the order of the triplets does not matter.
  * Example 2:
- *
+ * <p>
  * Input: nums = [0,1,1]
  * Output: []
  * Explanation: The only possible triplet does not sum up to 0.
  * Example 3:
- *
+ * <p>
  * Input: nums = [0,0,0]
  * Output: [[0,0,0]]
  * Explanation: The only possible triplet sums up to 0.
- *
  */
 
 public class SumOfTripletEqualsToZero {
     public static void main(String[] args) {
-
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println("threeSum(nums) = " + threeSum(nums));
     }
+
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
         List<List<Integer>> list = new ArrayList<>();
-        for (int i = 0; i < n-2; i++) {
-            if()
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            } else {
+                int left = i + 1;
+                int right = n - 1;
+                int sum = -nums[i];
+                while (left < right) {
+                    int current = nums[left] + nums[right];
+                    if (sum == current) {
+                        List<Integer> found = new ArrayList<>();
+                        found.add(nums[left]);
+                        found.add(nums[right]);
+                        list.add(found);
+                        left++;
+                        right--;
+                        while (left < n - 1 && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (right > i && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (current < sum) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
         }
-
+        return list;
     }
 }
