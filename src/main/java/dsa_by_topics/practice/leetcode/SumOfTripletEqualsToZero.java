fab3c80444304c39657e04dddbecc1e9c58b363c
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-16 22:55:12 +0530

    Two pointer practice

---
+++ src/main/java/dsa_by_topics/practice/leetcode/SumOfTripletEqualsToZero.java
@@ -0,0 +1,16 @@
+package dsa_by_topics.practice.leetcode;
+
+import java.util.List;
+
+/**
+ * Leetcode :  15. 3Sum
+ */
+
+public class SumOfTripletEqualsToZero {
+    public static void main(String[] args) {
+
+    }
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+    }
+}
