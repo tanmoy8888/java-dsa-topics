dc1bdb3241278d16bd86d1418fdd470308d1d397
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-16 22:58:37 +0530

    Two pointer practice

--- src/main/java/dsa_by_topics/practice/leetcode/SumOfTripletEqualsToZero.java
+++ src/main/java/dsa_by_topics/practice/leetcode/SumOfTripletEqualsToZero.java
@@ -1,11 +1,34 @@
 package dsa_by_topics.practice.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Leetcode :  15. 3Sum
  *
  * Link  :  https://leetcode.com/problems/3sum/description/
+ *
+ * Example 1:
+ *
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ * Explanation:
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+ * The distinct triplets are [-1,0,1] and [-1,-1,2].
+ * Notice that the order of the output and the order of the triplets does not matter.
+ * Example 2:
+ *
+ * Input: nums = [0,1,1]
+ * Output: []
+ * Explanation: The only possible triplet does not sum up to 0.
+ * Example 3:
+ *
+ * Input: nums = [0,0,0]
+ * Output: [[0,0,0]]
+ * Explanation: The only possible triplet sums up to 0.
  *
  */
 
@@ -14,6 +37,12 @@ public class SumOfTripletEqualsToZero {
 
     }
     public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n-2; i++) {
+            if()
+        }
 
     }
 }
