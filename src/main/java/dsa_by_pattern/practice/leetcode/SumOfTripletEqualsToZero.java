a7c24cdb0e42de09125016a555de90c56c69280e
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-17 20:56:41 +0530

    two pointer practice

--- src/main/java/dsa_by_pattern/practice/leetcode/SumOfTripletEqualsToZero.java
+++ src/main/java/dsa_by_pattern/practice/leetcode/SumOfTripletEqualsToZero.java
@@ -42,7 +42,6 @@ public class SumOfTripletEqualsToZero {
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        System.out.println("Arrays.toString(nums) = " + Arrays.toString(nums));
         List<List<Integer>> list = new ArrayList<>();
         for (int i = 0; i < n - 2; i++) {
             if (i > 0 && (nums[i-1] == nums[i])) {
