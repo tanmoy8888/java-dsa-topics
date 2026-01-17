3b92d91198453d3f5c8e5ff8f0214fd415e47948
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-17 23:03:51 +0530

    two pointer practice

--- src/main/java/dsa_by_pattern/practice/two_pointer/gfg/TripletsWithSmallerSum.java
+++ src/main/java/dsa_by_pattern/practice/two_pointer/gfg/TripletsWithSmallerSum.java
@@ -1,5 +1,7 @@
 package dsa_by_pattern.practice.two_pointer.gfg;
 
+import java.util.Arrays;
+
 public class TripletsWithSmallerSum {
     public static void main(String[] args) {
         int n = 4;
@@ -9,6 +11,22 @@ public class TripletsWithSmallerSum {
     }
 
     private static long countTriplets(int n, int sum, long arr[]) {
-
+        long value = 0;
+        long count = 0;
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                value = arr[i] + arr[left] + arr[right];
+                if (value >= sum) {
+                    right--;
+                } else {
+                    count = count + (right - left);
+                    left++;
+                }
+            }
+        }
+        return count;
     }
 }
