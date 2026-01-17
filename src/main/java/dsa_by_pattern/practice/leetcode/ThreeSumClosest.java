75eaa3a648496ccd5de40d7c86ddf28035519eb4
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-17 20:53:31 +0530

    two pointer practice

---
+++ src/main/java/dsa_by_pattern/practice/leetcode/ThreeSumClosest.java
@@ -0,0 +1,10 @@
+package dsa_by_pattern.practice.leetcode;
+
+public class ThreeSumClosest {
+    public static void main(String[] args) {
+
+    }
+    public static int threeSumClosest(int[] nums, int target) {
+
+    }
+}
