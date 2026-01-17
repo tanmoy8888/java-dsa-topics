59984c341285519b54b06cb138d62b45e4ea6125
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-17 20:55:45 +0530

    two pointer practice

--- src/main/java/dsa_by_pattern/practice/leetcode/ThreeSumClosest.java
+++ src/main/java/dsa_by_pattern/practice/leetcode/ThreeSumClosest.java
@@ -1,42 +1,42 @@
 package dsa_by_pattern.practice.leetcode;
 
 /**
- *  Leetcode 16. 3Sum Closest
- *
- *  Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums such that the sum is closest to target.
- *
+ * Leetcode 16. 3Sum Closest
+ * <p>
+ * Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums such that the sum is closest to target.
+ * <p>
  * Return the sum of the three integers.
- *
+ * <p>
  * You may assume that each input would have exactly one solution.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: nums = [-1,2,1,-4], target = 1
  * Output: 2
  * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
  * Example 2:
- *
+ * <p>
  * Input: nums = [0,0,0], target = 1
  * Output: 0
  * Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
- *
- *
+ * <p>
+ * <p>
  * Constraints:
- *
+ * <p>
  * 3 <= nums.length <= 500
  * -1000 <= nums[i] <= 1000
  * -104 <= target <= 104
- *
- *
  */
 
 public class ThreeSumClosest {
     public static void main(String[] args) {
-    int [] nums ={-1,2,1,-4};
-    int target = 1;
+        int[] nums = {-1, 2, 1, -4};
+        int target = 1;
+        System.out.println("threeSumClosest(nums,target) = " + threeSumClosest(nums, target));
     }
+
     public static int threeSumClosest(int[] nums, int target) {
 
     }
