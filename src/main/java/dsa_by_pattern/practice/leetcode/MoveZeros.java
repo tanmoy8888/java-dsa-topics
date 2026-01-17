3def7dd19ed5ab8d52341f40c3864de256cd6515
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-17 21:54:01 +0530

    two pointer practice

--- src/main/java/dsa_by_pattern/practice/leetcode/MoveZeros.java
+++ src/main/java/dsa_by_pattern/practice/leetcode/MoveZeros.java
@@ -1,7 +1,14 @@
 package dsa_by_pattern.practice.leetcode;
 
+/**
+ *  Leetcode 283. Move Zeroes
+ *
+ *  https://leetcode.com/problems/move-zeroes/description/
+ *  
+ */
+
 public class MoveZeros {
     public static void main(String[] args) {
-        
+
     }
 }
