ffdf1f2ad26c688283fdf1122ef765807d44c372
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-18 21:36:59 +0530

    two pointer practice

--- src/main/java/dsa_by_pattern/_02_sliding_window/gfg/_01_MaxSumSubarrayOfSizeK.java
+++ src/main/java/dsa_by_pattern/_02_sliding_window/gfg/_01_MaxSumSubarrayOfSizeK.java
@@ -2,15 +2,15 @@ package dsa_by_pattern._02_sliding_window.gfg;
 
 /**
  * GFG : https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
- *
+ * <p>
  * Max Sum Subarray of size K
  * Difficulty: EasyAccuracy: 49.6%Submissions: 246K+Points: 2
  * Given an array of integers arr[]  and a number k. Return the maximum sum of a subarray of size k.
- *
+ * <p>
  * Note: A subarray is a contiguous part of any given array.
- *
+ * <p>
  * Examples:
- *
+ * <p>
  * Input: arr[] = [100, 200, 300, 400], k = 2
  * Output: 700
  * Explanation: arr2 + arr3 = 700, which is maximum.
@@ -28,10 +28,31 @@ package dsa_by_pattern._02_sliding_window.gfg;
 
 public class _01_MaxSumSubarrayOfSizeK {
     public static void main(String[] args) {
-
+       /* int[] arr = {100, 200, 300, 400};
+        int k = 2;
+        */
+        //int k = 1;
+        int[] arr = {1, 4, 2, 10, 23, 3, 1, 0, 20};
+        int k = 4;
+        System.out.println("maxSubarraySum(arr,k) = " + maxSubarraySum(arr, k));
     }
-    public int maxSubarraySum(int[] arr, int k) {
-        // Code here
 
+    public static int maxSubarraySum(int[] arr, int k) {
+        int low = 0;
+        int high = k - 1;
+        int sum = 0;
+        int max_sum = 0;
+        for (int i = 0; i <= high; i++) {
+            sum = sum + arr[i];
+        }
+        while (high < arr.length) {
+            max_sum = Math.max(max_sum, sum);
+            low++;
+            high++;
+            if (high == arr.length) break;
+            sum = sum - arr[low - 1];
+            sum = sum + arr[high];
+        }
+        return max_sum;
     }
 }
