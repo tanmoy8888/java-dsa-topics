55ab167145a5ff07d9910c679c07918408c1e878
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-18 13:20:49 +0530

    two pointer practice

--- src/main/java/dsa_by_pattern/practice/two_pointer/leetcode/DutchNationalFlag.java
+++ src/main/java/dsa_by_pattern/practice/two_pointer/leetcode/DutchNationalFlag.java
@@ -1,7 +1,14 @@
 package dsa_by_pattern.practice.two_pointer.leetcode;
 
+/**
+ *  Leetcode :  75. Sort Colors
+ */
+
 public class DutchNationalFlag {
     public static void main(String[] args) {
+
+    }
+    public static void sortColors(int[] nums) {
 
     }
 }
