7e76bf2b872f1ea8a193abe97833504d4e660b5b
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-18 13:38:38 +0530

    two pointer practice

--- src/main/java/dsa_by_pattern/practice/two_pointer/leetcode/DutchNationalFlag.java
+++ src/main/java/dsa_by_pattern/practice/two_pointer/leetcode/DutchNationalFlag.java
@@ -1,5 +1,7 @@
 package dsa_by_pattern.practice.two_pointer.leetcode;
 
+import java.util.Arrays;
+
 /**
  * Leetcode :  75. Sort Colors
  * <p>
@@ -10,14 +12,25 @@ public class DutchNationalFlag {
     public static void main(String[] args) {
         int[] nums = {2, 0, 2, 1, 1, 0};
         sortColors(nums);
-
     }
 
     public static void sortColors(int[] nums) {
         int low = 0;
         int middle = 0;
         int high = nums.length - 1;
-
+        while (middle <= high) {
+            if (nums[middle] == 0) {
+                swap(nums, middle, low);
+                low++;
+                middle++;
+            } else if (nums[middle] == 2) {
+                swap(nums, middle, high);
+                high--;
+            } else {
+                middle++;
+            }
+        }
+        System.out.println("Arrays.toString(nums) = " + Arrays.toString(nums));
     }
 
     private static void swap(int[] nums, int pos1, int pos2) {
