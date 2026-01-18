c9a77bc9c15cc45ec7d35d3c5efe45e3797c08bf
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-18 20:46:45 +0530

    two pointer practice

--- src/main/java/dsa_by_pattern/sliding_window/MaximumSumOfKLengthSubArray.java
+++ src/main/java/dsa_by_pattern/sliding_window/MaximumSumOfKLengthSubArray.java
@@ -2,6 +2,9 @@ package dsa_by_pattern.sliding_window;
 
 public class MaximumSumOfKLengthSubArray {
     public static void main(String[] args) {
-        
+
+    }
+    public static long maximumSubarraySum(int[] nums, int k) {
+
     }
 }
