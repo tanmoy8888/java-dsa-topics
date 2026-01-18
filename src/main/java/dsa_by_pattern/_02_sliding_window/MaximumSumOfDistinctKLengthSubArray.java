ee104c97504da209136d7bfa5994ad236f7e26b8
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-01-18 21:28:10 +0530

    two pointer practice

---
+++ src/main/java/dsa_by_pattern/_02_sliding_window/MaximumSumOfDistinctKLengthSubArray.java
@@ -0,0 +1,11 @@
+package dsa_by_pattern._02_sliding_window;
+
+public class MaximumSumOfDistinctKLengthSubArray {
+    public static void main(String[] args) {
+
+    }
+
+    public static long maximumSubarraySum(int[] nums, int k) {
+        return 0;
+    }
+}
---
+++ src/main/java/dsa_by_pattern/_02_sliding_window/gfg/_01_MaxSumSubarrayOfSizeK.java
@@ -0,0 +1,37 @@
+package dsa_by_pattern._02_sliding_window.gfg;
+
+/**
+ * GFG : https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
+ *
+ * Max Sum Subarray of size K
+ * Difficulty: EasyAccuracy: 49.6%Submissions: 246K+Points: 2
+ * Given an array of integers arr[]  and a number k. Return the maximum sum of a subarray of size k.
+ *
+ * Note: A subarray is a contiguous part of any given array.
+ *
+ * Examples:
+ *
+ * Input: arr[] = [100, 200, 300, 400], k = 2
+ * Output: 700
+ * Explanation: arr2 + arr3 = 700, which is maximum.
+ * Input: arr[] = [1, 4, 2, 10, 23, 3, 1, 0, 20], k = 4
+ * Output: 39
+ * Explanation: arr1 + arr2 + arr3 + arr4 = 39, which is maximum.
+ * Input: arr[] = [100, 200, 300, 400], k = 1
+ * Output: 400
+ * Explanation: arr3 = 400, which is maximum.
+ * Constraints:
+ * 1 ≤ arr.size() ≤ 106
+ * 1 ≤ arr[i] ≤ 106
+ * 1 ≤ k ≤ arr.size()
+ */
+
+public class _01_MaxSumSubarrayOfSizeK {
+    public static void main(String[] args) {
+
+    }
+    public int maxSubarraySum(int[] arr, int k) {
+        // Code here
+
+    }
+}
